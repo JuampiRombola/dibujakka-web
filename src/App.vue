c33ef7d70636cf5674dead97c345e19656dd62c1
JuampiRombola <juampi.rombola@gmail.com>
2021-06-13 11:30:14 -0300

    Add Login component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+        app
+        color="white"
+        flat
+        dense
+    >
+      <v-avatar
+          :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+          size="32"
+      ></v-avatar>
+
+      <v-avatar
+          class="hidden-sm-and-down"
+          color="grey darken-1 shrink"
+          size="32"
+      ></v-avatar>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data: () => ({
+    links: [
+      'Dashboard',
+      'Messages',
+      'Profile',
+      'Updates',
+    ],
+  }),
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- src/views/Match.vue
+++ src/views/Match.vue
@@ -1,10 +1,41 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="8">
-        <DrawingTool></DrawingTool>
+      <v-col
+          cols="12"
+          sm="2"
+      >
+        <v-sheet
+            rounded="lg"
+            min-height="268"
+        >
+          <!--  -->
+        </v-sheet>
       </v-col>
-      <v-col cols="4">chat</v-col>
+
+      <v-col
+          cols="12"
+          sm="7"
+      >
+        <v-sheet
+            min-height="70vh"
+            rounded="lg"
+        >
+          <DrawingTool></DrawingTool>
+        </v-sheet>
+      </v-col>
+
+      <v-col
+          cols="12"
+          sm="3"
+      >
+        <v-sheet
+            rounded="lg"
+            min-height="268"
+        >
+          <!--  -->
+        </v-sheet>
+      </v-col>
     </v-row>
   </v-container>
 </template>
