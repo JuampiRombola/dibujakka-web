60f08c9d1bae7bad1d5893f5f3ad6fbfec5eb41f
JuampiRombola <juampi.rombola@gmail.com>
2021-06-13 01:38:23 -0300

    Initial drawing tool

---
+++ src/views/Match.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="8">
+        <DrawingTool></DrawingTool>
+      </v-col>
+      <v-col cols="4">chat</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import DrawingTool from "@/components/DrawingTool";
+
+export default {
+  components: {
+    DrawingTool
+  },
+
+  name: "Match"
+}
+</script>
+
+<style scoped>
+
+</style>
