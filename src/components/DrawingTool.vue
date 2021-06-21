af27af24c573fa0cc3e312e5c658deea575779f3
Juampi Rombolá <juampi.rombola@gmail.com>
2021-06-21 17:37:30 -0300

    Add buttons and style to DrawingTool

--- src/components/DrawingTool.vue
+++ src/components/DrawingTool.vue
@@ -1,15 +1,52 @@
 <template>
   <div>
     <Editor class="drawing-canvas" :canvasWidth="500" :canvasHeight="500" ref="editor"/>
-    <v-btn @click="freeDrawing()" icon>
-      <v-icon :color="colorIfSelected('freeDrawing')">mdi-pencil</v-icon>
-    </v-btn>
-    <v-btn @click="eraser()" icon>
-      <v-icon :color="colorIfSelected('eraser')">mdi-eraser</v-icon>
-    </v-btn>
-    <v-btn @click="clear()" icon>
-      <v-icon color="grey">mdi-delete</v-icon>
-    </v-btn>
+    <div class="text-center">
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="200"
+        offset-x
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on" class="mx-2">
+            <v-icon large :color="strokeColor">mdi-circle</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-color-picker
+            v-model="strokeColor"
+            dot-size="25"
+            swatches-max-height="200"
+            hide-inputs
+          ></v-color-picker>
+          <v-slider
+            v-model="strokeWidth"
+            min="1"
+            max="20"
+            color="grey darken-1"
+            thumb-color="black"
+            class="pl-5 pr-3"
+            label="Line"
+            :thumb-size="1.8*strokeWidth"
+          >
+            <template v-slot:thumb-label="{ value }">
+              <v-icon color="black" :size="1.8*value">mdi-circle</v-icon>
+            </template>
+          </v-slider>
+        </v-card>
+      </v-menu>
+      <v-btn @click="freeDrawing()" icon class="mx-2">
+        <v-icon large :color="colorIfSelected('freeDrawing')">mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn @click="eraser()" icon class="mx-2">
+        <v-icon large :color="colorIfSelected('eraser')">mdi-eraser</v-icon>
+      </v-btn>
+      <v-btn @click="clear()" icon class="mx-2">
+        <v-icon large color="grey">mdi-delete</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -25,9 +62,10 @@ export default {
 
   data: () => ({
     pollingInterval: undefined,
-    strokeColor: 'black',
+    strokeColor: '#000000',
     strokeWidth: 2,
-    currentTool: 'freeDrawing'
+    currentTool: 'freeDrawing',
+    menu: false
   }),
 
   computed: {
@@ -68,6 +106,15 @@ export default {
 
   mounted() {
     this.enableDrawingMode()
+  },
+
+  watch: {
+    strokeColor () {
+      this.freeDrawing()
+    },
+    strokeWidth () {
+      this.freeDrawing()
+    }
   }
 }
 </script>
