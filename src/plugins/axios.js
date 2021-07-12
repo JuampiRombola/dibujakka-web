be6b481e19e2e2e16b03177b92bbf1218e59d80d
Juampi Rombolá <juampi.rombola@gmail.com>
2021-07-12 19:56:57 -0300

    Change base url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
-axios.defaults.baseURL = process.env.VUE_APP_BASE_URL || process.env.apiUrl || 'http://dibujakka-webserver.herokuapp.com';
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL || process.env.apiUrl || 'http://dibujakka-server.herokuapp.com';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
